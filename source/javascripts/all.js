e2782f7bb7b1b5b2fb21c0b656eeecdc486e5fbc
Franklin Webber <franklin.webber@gmail.com>
2013-06-26 10:48:26 -0600

    Auto resize email student contact info when it is long

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,21 @@
 //= require_tree .
+
+
+$(function() {
+  resizeStudentContactInfo();
+});
+
+var resizeStudentContactInfo = function() {
+
+  $(".student-contact-info span").each(function(index) {
+    var currentWidth = parseInt($(this).width());
+
+    while(currentWidth > 150) {
+      var currentFontSize = parseInt( $(this).css('font-size') );
+      var newFontSize = (currentFontSize - 1) + "px";
+      $(this).css('font-size',newFontSize);
+      currentWidth = parseInt( $(this).width() );
+    }
+  });
+
+}
