7584af0872949c2ae677ceeee14f534d1f613ef2
edatrix <lespath@gmail.com>
2014-01-29 16:16:55 -0700

    first crack at portfolio

---
+++ data/students/spath_luhring_lauren.rb
@@ -0,0 +1,34 @@
+{
+  :last_name => "Spath Luhring",
+  :first_name => "Lauren",
+  :image_url => "/images/students/gschool1/lauren.jpg",
+  :email_address => "lespath@gmail.com",
+  :looking_for_description => "I am ready to sink my teeth into a fun, impactful project with other smart, driven people.  I'd love to be a part of an agile team that supports continuous learning, drives development with tests, and laughs a lot while doing so.",
+  :best_at => ['writing clean code', 'problem solving', 'project management', 'timely animated gifs'],
+  :looking_for => ['continuous learning', 'agile environment', 'new challenges every day', 'having fun'],
+  :locations => ["Boulder, CO", "Denver, CO"],
+  :code => {
+    :github => 'https://github.com/edatrix',
+    :project_1 => {
+      :title => "Runline",
+      :description => "Runline enables users to track activities with a personalized dashboard and feed of their runs and to follow and compare data with friends. Learning focused on consumption of third-party APIs, use of background workers, TDD at all levels of development, and a fantastic user experience with an emphasis on performance.",
+      :url => 'https://github.com/edatrix/runline',
+      :what_to_look_for => ['a gorgeous custom front end', 'significant test coverage', 'an API wrapper gem that maintain separation between Runline and third-party APIs ']
+    },
+    :project_2 => {
+      :title => "IdeaBox",
+      :description => "IdeaBox is a simple web application (my very first!) that allows users to record ideas.  Built using Ruby, this project was an introduction to Sinatra and an experiment in front-end design.",
+      :url => 'https://github.com/edatrix/idea-box',
+      :what_to_look_for => ['integration with SMS service Twilio']
+    },
+    :project_3 => {
+      :title => "HaxHub - Integrating the APIs of Github, Pivotal Tracker, and Travis CI to serve as an all-in-one software project feed.",
+      :description => "This was a fun experience trying to create a single-page application that aggregated a external content in real-time. We integrated WebHooks as well as some basic jQuery/CoffeeScript polling for new data.",
+      :url => 'https://github.com/gitbhub_username/haxhub',
+      :what_to_look_for => ['Consuming data through webhook', 'Good extraction of data extraction through APIs', 'Clean Implementation of jQuery for polling']
+    }
+  },
+  :resume => "http://cl.ly/3G3z0a0f0m0V",
+  :references => ['Jeff Casimir', 'Rachel Lubin'],
+  :other_notes => ""
+}
