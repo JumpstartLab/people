06e90ee15e0f880c2e4ae092dc3d41a202fce141
Bryana Knight <brknig11@gmail.com>
2014-01-30 14:43:50 -0700

    edit projects and about me

--- data/students/knight_bryana.rb
+++ data/students/knight_bryana.rb
@@ -4,22 +4,22 @@
   :image_url => "/images/students/gschool1/bryana.jpg",
   :email_address => "bryana.r.knight@gmail.com",
   :looking_for_description => "",
-  :best_at => ['Testing First', 'Writing Clean Code', 'Getting Things Done!'],
-  :looking_for => ["Continuous Learning", 'Building Products', "Having Fun"],
+  :best_at => ['Ruby', 'Rails', 'Collaboration', 'BDD'],
+  :looking_for => ["Continuous Learning", "Building products I believe in", "Collaborative work environment"],
   :locations => ["Seattle, WA", "Boston, MA", "Washington, DC"],
   :code => {
     :github => 'https://github.com/BryanaKnight',
     :project_1 => {
       :title => "MileHigh",
-      :description => "A version of Yelp for legal Marijuana stores in Colorado. We authenticated through Facebook, wrote a gem to expose our own API and another to consume the Leafly API. We used JavaScript and jQuery to produce a dynamic map to display store locations of results from a seach.",
+      :description => "A version of Yelp for legal Marijuana stores in Colorado. We authenticated through Facebook, wrote a gem to expose our own API and another to consume the Leafly API.",
       :url => 'https://github.com/BryanaKnight/milehigh',
-      :what_to_look_for => ['HyperMedia API endpoint at /api/v1', 'use of FactoryGirl', 'JavaScript and jQuery implementation']
+      :what_to_look_for => ['webscraping using Nokogiri', 'database indexing', 'JavaScript and jQuery implementation']
     },
     :project_2 => {
-      :title => "RunTalk - a mobile chat system",
-      :description => "RunTalk is a mobile chat system. It uses services to break up the architecture into more manageable pieces.",
-      :url => 'http://github.com/gitbhub_username/runtalk',
-      :what_to_look_for => ['You get services!', 'and you get services!', 'and you get services!']
+      :title => "Fourth Meal",
+      :description => "Worked with a legacy codebase of an individual restaurant online ordering system to build a restaurant platform capable of hosting thousands of restaurants.",
+      :url => 'http://github.com/BryanaKnight/fourth_meal',
+      :what_to_look_for => ['background workers', "'Russian Doll' fragment caching", 'scalable architecture']
     },
     :project_3 => {
       :title => "HaxHub - Integrating the APIs of Github, Pivotal Tracker, and Travis CI to serve as an all-in-one software project feed.",
