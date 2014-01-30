12a3a00a0810cfa5e11ea93da4317df847c5664a
Bryana Knight <brknig11@gmail.com>
2014-01-30 13:01:59 -0700

    edit first project

---
+++ data/students/knight_bryana.rb
@@ -0,0 +1,34 @@
+{
+  :last_name => "Knight",
+  :first_name => "Bryana",
+  :image_url => "/images/students/gschool1/bryana.jpg",
+  :email_address => "bryana.r.knight@gmail.com",
+  :looking_for_description => "",
+  :best_at => ['Testing First', 'Writing Clean Code', 'Getting Things Done!'],
+  :looking_for => ["Continuous Learning", 'Building Products', "Having Fun"],
+  :locations => ["Seattle, WA", "Boston, MA", "Washington, DC"],
+  :code => {
+    :github => 'https://github.com/BryanaKnight',
+    :project_1 => {
+      :title => "MileHigh",
+      :description => "A version of Yelp for legal Marijuana stores in Colorado. We authenticated through Facebook, wrote a gem to expose our own API and another to consume the Leafly API. We used JavaScript and jQuery to produce a dynamic map to display store locations of results from a seach.",
+      :url => 'https://github.com/BryanaKnight/milehigh',
+      :what_to_look_for => ['HyperMedia API endpoint at /api/v1', 'use of FactoryGirl', 'JavaScript and jQuery implementation']
+    },
+    :project_2 => {
+      :title => "RunTalk - a mobile chat system",
+      :description => "RunTalk is a mobile chat system. It uses services to break up the architecture into more manageable pieces.",
+      :url => 'http://github.com/gitbhub_username/runtalk',
+      :what_to_look_for => ['You get services!', 'and you get services!', 'and you get services!']
+    },
+    :project_3 => {
+      :title => "HaxHub - Integrating the APIs of Github, Pivotal Tracker, and Travis CI to serve as an all-in-one software project feed.",
+      :description => "This was a fun experience trying to create a single-page application that aggregated a external content in real-time. We integrated WebHooks as well as some basic jQuery/CoffeeScript polling for new data.",
+      :url => 'https://github.com/gitbhub_username/haxhub',
+      :what_to_look_for => ['Consuming data through webhook', 'Good extraction of data extraction through APIs', 'Clean Implementation of jQuery for polling']
+    }
+  },
+  :resume => "http://blairbuilds.herokuapp.com/resume.html",
+  :references => ['Jeff Casimir', 'Geoffrey Schorkopf, Jorge Tellez'],
+  :other_notes => "I am Awesome. Find me on twitter at @sinatrarb"
+}
