85653913168fa665e8a6ea041883b86d5d248c78
Nathaniel Watts <reg@nathanielwatts.com>
2014-01-31 16:03:59 -0700

    add myself

---
+++ data/gschool1/watts_nathaniel.rb
@@ -0,0 +1,58 @@
+{
+  :last_name => "Nathaniel",
+  :first_name => "Watts",
+  :image_url => "/images/students/gschool1/watts.jpg",
+  :email_address => "watts@nathanielwatts.com",
+  :looking_for_description => "I don't just want to settle with being a decent programmer - I want to be an expert of my craft. I love learning, being challenged, and working on passionate - driven - teams. My desire is to work in an environment that solves real problems with sustainable solutions, and values teammates both in and out of the office.",
+  :best_at => ['Test Driven Development', 'Solving Real Problems with Software', 'Teamwork / Teambuilding'],
+  :looking_for => ["Best Practices", "Continuous Learning", "Passion for Excellence", "Great Team Dynamics & Culture"],
+  :locations => ["Boulder, CO", "Denver, CO", "Dallas, TX", "Chicago, IL", "Remote"],
+  :code => {
+    :github => 'https://github.com/thewatts',
+    :project_1 => {
+      :title => "FooFoBerry - A real-time dashboard for software teams and clients to use to track progress and momentum.",
+      :description => "Our team collaborated to build a our product using a Service Oriented Architecture approach. Altogether we built three Rails applications, an application built using Sinatra to receive webhook callbacks from GitHub and Pivotal Tracker, as well as an Ember.js application that listened to [Pusher](http://pusher.com/) channels to post real-time updates. We tied all these together using a gem to interface with our API, as well as Nginx and Phusion Passenger on a VPS.",
+      :url => 'https://github.com/FooFoBerry',
+      :what_to_look_for => [
+        'Service Oriented Architecture',
+        'High test coverage',
+        'API created with the Rails API gem',
+        'Ember.js'
+      ]
+    },
+    :project_2 => {
+      :title => "Powwow.io - a team idea submission platform.",
+      :description => "The business logic was implemeneted in pure Ruby. Sinatra was added to serve a web interface, and I built a simple gem to wrap the Sinatra API. Technologies include: Ruby, Sinatra, JavaScript, Pusher, Haml, TDD with MiniTest and Rack/Test",
+      :url => 'http://github.com/thewatts/powwow.io',
+      :what_to_look_for => [
+        'API Endpoints',
+        'Pusher Integration',
+        'Accompanying Gem: github.com/thewatts/powwow'
+      ]
+    },
+    :project_3 => {
+      :title => "A* Search Algorithm - an exploration of documenting my process implementing A* in Ruby using TDD.",
+      :description => "My goal for this personal project is to write blog posts detailing my process of learning how to implement the A* Search Algorithm using Ruby. Currently a work in progress.",
+      :url => 'https://github.com/thewatts/a_star',
+      :what_to_look_for => [
+        'TDD',
+        'Writing'
+      ]
+    },
+    :project_4 => {
+      :title => "Refactor Tractor - Image Urls - an exploration in refactoring.",
+      :description => "My goal for this personal refactoring project was to take some real world code that I'd written, and refactor it to make more succinct and effective.",
+      :url => 'https://github.com/thewatts/refactor-tractor/tree/master/ruby/image-urls',
+      :what_to_look_for => [
+        'Refactoring the original to v1',
+        'Lambdas',
+        'Code reduction',
+        'Meta Programming'
+      ]
+    }
+  },
+  :resume => "http://nathanielwatts.com/resume.html",
+  :references => ['Jeff Casimir', 'Katrina Owen'],
+  :other_notes => "I am Awesome. Find me on twitter at @sinatrarb",
+  :group => 'gschool1'
+}
