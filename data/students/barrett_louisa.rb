0649106db6341f5c1edcf611b0add17269172a11
Louisa Barrett <louisabarrett@gmail.com>
2014-02-06 19:03:09 -0700

    content edits

--- data/students/barrett_louisa.rb
+++ data/students/barrett_louisa.rb
@@ -5,15 +5,15 @@
   :email_address => "louisabarrett@gmail.com",
   :looking_for_description => "I'm on the hunt for a great team building great products. I thrive in tight-knit, collaborative environments and love working with dedicated, driven teammates. I'm passionate about education, conservation, healthcare, design and great user experiences. I continually seek out ways to learn new skills and push myself outside of my comfort zone, and I take great pride in doing the best work possible.",
   :best_at => ['Pair programming', 'TDD', 'Attention to detail and love of well crafted code'],
-  :looking_for => ["Continued learning and growth as a developer", 'Supportive team environment', "A blend of front- and back-end responsibilities"], 
+  :looking_for => ["Continued learning and growth as a developer", 'Supportive team environment', "A blend of front and back-end responsibilities"], 
   :locations => ["Denver, CO", "Boulder, CO", "San Francisco, CA", "Seatle, WA", "Boston, MA", "Washington, D.C."],
   :code => {
     :github => 'https://github.com/LouisaBarrett',
     :project_1 => {
       :title => "Runline 2.0",
-      :description => "This was a project in refactoring. As part of a two person team, we challenged ourselves to implement object oriented programming and single responsibility principles to an extreme and dive deeply into OOP concepts. We focused on taking small steps, keeping our test suite running green, and creating a variety of service objects that adhere to SRP.",
+      :description => "This was a project in refactoring. As part of a two person team, we challenged ourselves to implement object oriented programming and single responsibility principles to extremes and dive deeply into OOP concepts. We focused on taking small steps, keeping our test suite running green, and creating a variety of service objects that adhere to SRP.",
       :url => 'https://github.com/LouisaBarrett/runline-2',
-      :what_to_look_for => ['Test coverage', 'Objects with clearly defined, single responsibilities', 'Object Oriented Programming principles']
+      :what_to_look_for => ['Test coverage', 'Objects with clearly defined responsibilities', 'Object Oriented Programming principles']
     },
     :project_2 => {
       :title => "FundIt - a fund-raising app",
@@ -25,7 +25,7 @@
       :title => "Command Query - Solutions to a series of kata-like tests practicing how objects change in Ruby.",
       :description => "This was a fun group of bite-sized, foundational Ruby problems to work through, ranging in difficulty from simple to tricky.",
       :url => 'https://github.com/LouisaBarrett/command-query_practice',
-      :what_to_look_for => ['Consistancy of solutions', 'Solid understanding of changing the state of an object']
+      :what_to_look_for => ['Consistency of solutions', 'Solid understanding of changing the state of an object']
     }
   },
   :resume => "http://f.cl.ly/items/2D2S2u0L0Z2B3x0o3X1Y/LouisaBarrett_resume.pdf",
