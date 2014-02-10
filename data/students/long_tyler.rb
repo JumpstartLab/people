ec7e9f9936ded26fb1ab2061f32fb3c96914a180
Tyler Long <tyler.stephen.long@gmail.com>
2014-02-10 08:36:03 -0700

    fix typos and update a few things

--- data/students/long_tyler.rb
+++ data/students/long_tyler.rb
@@ -5,25 +5,25 @@
   :email_address => "tyler.stephen.long@gmail.com",
   :looking_for_description => "I am not only looking for a company that will make me a better programmer, but also a solid team that values TDD, agile development, and a strong commitment to learning. ",
   :best_at => ['Testing', 'Ruby', 'Rails', 'Problem Solving', 'Collaboration'],
-  :looking_for => ["Full stack development", "Pair Programming", "Exposure to different programming languages"],
+  :looking_for => ["Full Stack Development", "Pair Programming", "Exposure to different programming languages"],
   :locations => ["Berlin, DE", "New York, NY", "Sydney, AU", "Seattle, WA", "Boulder, CO", "San Francisco, CA", "Portland, OR"],
   :code => {
     :github => 'https://github.com/Teapane',
     :project_1 => {
       :title => "FooFoBerry - A multi app dashboard to integrate Github, Pivotal Tracker and Travis CI.",
-      :description => "FooFoBerry is a service oriented designed application that acts as a live dashboard for commits, pivotal stories, and TravisCi build status.  The application runs on six seperate components: three of them built in rails, one ember.js front-end, a Ruby API-Gem, and a Sinatra callback receiver",
+      :description => "FooFoBerry is a service oriented application that acts as a live dashboard for commits, pivotal stories, and TravisCi build status.  The application runs on six separate components: three of them built in Rails, one Ember.js front-end, a Ruby API-Gem, and a Sinatra callback receiver",
       :url => 'http://github.com/FooFoBerry',
-      :what_to_look_for => ["'SOA Architecture", "API Gem", "RSpec test coverage"]
+      :what_to_look_for => ["SOA Architecture", "API Gem", "RSpec test coverage"]
     },
     :project_2 => {
-      :title => "Craveyard- A multitenancy App to facilitate online restaurant ordering",
-      :description => "Craveyard is an online restaurant ordering system that hosts 10,000 restaurants and 100,000 users. The app is built on top of a legacy code base, with added multitenancy and redis for a responsive interface.",
-      :url => 'http://github.com/gitbhub_username/runtalk',
+      :title => "Craveyard- A multitenancy app to facilitate online restaurant ordering",
+      :description => "Craveyard is an online restaurant ordering system that hosts 10,000 restaurants and 100,000 users. We added multitenancy to a legacy code base and added Redis for a responsive interface.",
+      :url => 'http://github.com/Teapane/fourth_meal',
       :what_to_look_for => ['Pagination', 'Caching, Redis', 'Stripe Payment extraction', 'Admin interface']
     },
     :project_3 => {
-      :title => "Planting Season - An interactive APP to help you plan your garden depending on space and plant hardiness zone",
-      :description => "An interactive serviece oriented app, built with rails and ember.js. ",
+      :title => "Planting Season - An interactive application to help you plan your garden depending on available space and where you live.  It also sends notifications about frost warnings, when to harvest, and companion planting",
+      :description => "An interactive service oriented app, built with Rails and tested with RSpec ",
       :url => 'https://github.com/PlantingSeason',
       :what_to_look_for => ['Interactive interface', 'Solid testing both in Javascript and Ruby']
     }
